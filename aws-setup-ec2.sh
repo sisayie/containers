ab296c6a6d333db3a30836e10ae571b9e6672261
Sisay Chala <sisayie@gmail.com>
2026-09-03 09:10:36 +0200

    readme updated

--- aws-setup-ec2.sh
+++ aws-setup-ec2.sh
@@ -1,3 +1,10 @@
+########################
+# Prepare the system
+########################
+
+#----------------------
+# 1. Update cache and instll aws cli
+#----------------------
 sudo apt update
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -5,13 +12,13 @@ sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
-aws iam attach-role-policy \
-  --role-name exam-score-ml-ExamScoreServerRole-PQb9nKLHVkn1 \
-  --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
+# aws iam attach-role-policy \
+#   --role-name exam-score-ml-ExamScoreServerRole-PQb9nKLHVkn1 \
+#   --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
 
-########################
-# Install Docker
-########################
+#------------------------
+# 2. Setup Docker
+#------------------------
 
 # Set up Docker's apt repository.
 # Add Docker's official GPG key:
@@ -49,13 +56,25 @@ newgrp docker
 #   --role-name exam-score-ml-ExamScoreServerRole-PQb9nKLHVkn1 \
 #   --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
 
+########################
+# Fetch and run the container
+########################
+
+#------------------------
+# 3. Authenticate docker on ECR
+#------------------------
+
 # Here you must 
 # Authenticate docker -> log docker into the registry
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 339713011628.dkr.ecr.us-east-1.amazonaws.com/exam-score-ml/exam-score-api
 
-## Run
+#------------------------
+# 4. Pull and run the container
+#------------------------
+
 docker run -d \
   --name my-app \
   -p 5000:5000 \
   #-e DOCKER_HOST=tcp://docker:2375 \
   339713011628.dkr.ecr.us-east-1.amazonaws.com/exam-score-ml/exam-score-api:latest
+
